a7cf5e214884e259d7ba7473671891cb967c4f83
mr-Dev-Anshu <anshur9608837@gmail.com>
2024-02-15 13:01:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function App() {
+    
+    
+    
+  return (
+    <div className='dark'>
+     {/* <button className='btn'>Click me </button>
+     <h1 className='dark1'>Hii</h1>
+     <h1 className='dark2'>Hii</h1>
+     <h1 className='dark3'>Hii</h1>
+     <h1 className='dark4'>Hii</h1>
+     <h1 className='dark5'>Hii</h1> */}
+     <button   className='btn'>Click me </button>
+      <div className='min-h-screen dark1 center-box    '>
+        <div className='card color   '>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, sit! Eaque doloremque exercitationem quaerat modi placeat nemo qui. Ipsum, natus provident sit veniam explicabo dolore iusto labore dignissimos quidem nostrum?
+        </div>
+      </div>
+    </div>
+  )
+}
