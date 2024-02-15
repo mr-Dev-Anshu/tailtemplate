59cd0136c52c22196564c4484339739fbb9ec153
mr-Dev-Anshu <anshur9608837@gmail.com>
2024-02-15 18:15:57 +0530

    test

--- src/App.js
+++ src/App.js
@@ -13,10 +13,12 @@ export default function App() {
      <h1 className='dark4'>Hii</h1>
      <h1 className='dark5'>Hii</h1> */}
      <button   className='btn'>Click me </button>
-      <div className='min-h-screen dark1 center-box    '>
+      <div className='min-h-screen dark1 center-box gap-2   '>
         <div className='card color   '>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, sit! Eaque doloremque exercitationem quaerat modi placeat nemo qui. Ipsum, natus provident sit veniam explicabo dolore iusto labore dignissimos quidem nostrum?
         </div>
+         
+          <div className='card color'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas distinctio quidem cumque nulla magni nostrum non, tempore ipsa asperiores nisi harum ullam voluptate!</div>
       </div>
     </div>
   )
